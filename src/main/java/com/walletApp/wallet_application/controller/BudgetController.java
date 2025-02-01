66b18e476c2959b4b9dd4c350855b8ce92cd580c
Arsene Nyirinkwaya <dellamis11@gmail.com>
2025-02-01 14:29:55 +0200

    created controller for budget and implemented methods for budget CRUD operations

---
+++ src/main/java/com/walletApp/wallet_application/controller/BudgetController.java
@@ -0,0 +1,68 @@
+package com.walletApp.wallet_application.controller;
+
+import com.walletApp.wallet_application.entity.Account;
+import com.walletApp.wallet_application.entity.Budget;
+import com.walletApp.wallet_application.service.BudgetService;
+import com.walletApp.wallet_application.service.dto.AccUpdateDto;
+import com.walletApp.wallet_application.service.dto.AccountDto;
+import com.walletApp.wallet_application.service.dto.BudgetDto;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/budgets")
+@RequiredArgsConstructor
+public class BudgetController {
+
+    private final BudgetService service;
+    private String jwtToken;
+
+    @GetMapping
+    public ResponseEntity<?> getAllBudget(
+            @NotNull @RequestHeader("Authorization") String token
+    ) {
+        jwtToken = token.replace("Bearer ", "");
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(service.budgetListByUser(jwtToken));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Budget> getBudgetById(
+            @NotNull @RequestHeader("Authorization") String token,
+            @PathVariable Long id) {
+        jwtToken = token.replace("Bearer ","");
+        Budget getBudgetDetailsById = service.getBudgetDetails(jwtToken, id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(getBudgetDetailsById);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> createBudget(
+            @NotNull @RequestHeader("Authorization") String token,
+            @RequestBody BudgetDto dto) {
+        jwtToken = token.replace("Bearer ", "");
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createBudget(jwtToken, dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateAccount(@NotNull @RequestHeader("Authorization") String token,
+                                           @PathVariable Long id,
+                                           @RequestBody BudgetDto updateDto) {
+        jwtToken = token.replace("Bearer ", "");
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.updateBudget(jwtToken, id, updateDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBudget(@NotNull @RequestHeader("Authorization") String token,
+                                           @PathVariable Long id) {
+        jwtToken = token.replace("Bearer ", "");
+        service.deleteBudget(jwtToken, id);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Delete Successfully");
+    }
+
+}
